37336c53143b681841e002672de4e26620e4217d
shantanuVoosh <shantanu@voosh.in>
2022-04-07 12:15:52 +0530

    first commit

---
+++ src/global/globalStyles.js
@@ -0,0 +1,14 @@
+export const globalFontStyle = {
+  // fontFamily: 'Roboto',
+};
+
+export const globalFontSize = {
+  // Example
+  //   sm: 8,
+  //   md: 16,
+};
+
+export const globalColor = {
+    // Example
+    //   primary: '#00bcd4',
+}
---
+++ src/global/media.js
@@ -0,0 +1,4 @@
+export const media = {
+  non_veg_icon: require('./media/non-veg.png'),
+  veg_icon: require('./media/veg.png'),
+};
