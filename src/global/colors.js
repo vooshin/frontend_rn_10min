d185f7a8db18a653349428037164eb4ca36939b2
shantanuVoosh <shantanu@voosh.in>
2022-04-07 14:32:52 +0530

    colors added

---
+++ src/global/colors.js
@@ -0,0 +1,47 @@
+export const colors = {
+  // Headers
+  header_bg_lightblue_1: '#83ADFF',
+  header_bg_lightblue_2: '#318BBE',
+
+  header_bg_blue_1: '#7451FF',
+  header_bg_blue_2: '#5A00ED',
+
+  header_bg_yellow_1: '#FFBA09',
+  header_bg_yellow_2: '#FF9900',
+
+  header_bg_red_1: '#FF7051',
+  header_bg_red_2: '#ED0055',
+
+  header_bg_purple_1: '#A851FF',
+  header_bg_purple_2: '#6800ED',
+
+  // Alert timer colors
+  timer_bg: '#2C2940',
+  timer_bg_lightblue: '#68CFFB',
+  timer_bg_yellow: '#FFB808',
+  timer_bg_red: '#FE6851',
+  timer_bg_purple: '#B469FF',
+  timer_bg_blue: '#849FFF',
+  timer_text_red: '#F84353',
+
+  //   Food tags (Best Value, Healthy, Best Seller, etc)
+  tag_bg_1: '#5C79C3',
+  tag_bg_2: '#5CC379',
+  tag_bg_3: '#ECB528',
+
+  // Notifications
+  notification_color_1: '#FFF09C',
+
+  // Buttons
+  button_bg_green_1: '#1CD14F',
+  button_bg_green_1: '#16A43E',
+  button_bg_border: '#D8D8D8',
+  button_bg_white_1: '#FFFFFF',
+  button_bg_white_2: '#D8D8D8',
+
+  color_back_1: '#2C2940',
+  color_back_1: '#000000',
+  color_white_1: '#FFFFFF',
+  color_white_2: '#F5F4F9',
+  color_white_3: '#C4C4C4',
+};
